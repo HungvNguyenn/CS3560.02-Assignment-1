a4392002bd59214692632a5adfcf9b9e064ab70d
Hung Nguyen <hungnguyen210703@gmail.com>
2025-02-26 20:41:26 -0800

    implemented ValidParentheses

--- src/main/java/org/example/ValidParentheses.java
+++ src/main/java/org/example/ValidParentheses.java
@@ -1,5 +1,21 @@
 package org.example;
 
-public class ValidParentheses {
+import java.util.Stack;
 
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c: s.toCharArray()) {
+            if(c == '('){
+                stack.push(')');
+            } else if(c == '{'){
+                stack.push('}');
+            } else if(c == '['){
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
 }
