dc93bd8d0520862c40724f8bad8ca33909787ce8
Hung Nguyen <hungnguyen210703@gmail.com>
2025-02-27 17:43:04 -0800

    implemented ValidParenthesesTest

--- src/test/java/org/example/ValidParenthesesTest.java
+++ src/test/java/org/example/ValidParenthesesTest.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidParenthesesTest {
+
+    @Test
+    public void testIsValid() {
+        ValidParentheses vp = new ValidParentheses();
+        assertTrue(vp.isValid("()"));
+        assertTrue(vp.isValid("()[]{}"));
+        assertFalse(vp.isValid("(]"));
+        assertFalse(vp.isValid("([)]"));
+    }
+
+    @Test
+    public void emptyString(){
+        ValidParentheses vp = new ValidParentheses();
+        assertTrue(vp.isValid(""));
+    }
+
+    @Test
+    public void singleBracket(){
+        ValidParentheses vp = new ValidParentheses();
+        assertFalse(vp.isValid("["));
+        assertFalse(vp.isValid("("));
+        assertFalse(vp.isValid("{"));
+    }
+
+    @Test
+    public void longStrings(){
+        ValidParentheses vp = new ValidParentheses();
+        assertTrue(vp.isValid("(((((((((())))))))))"));
+        assertTrue(vp.isValid("[{({})({})({}[])}]"));
+        assertFalse(vp.isValid("(((((((())))))))("));
+        assertFalse(vp.isValid("[{({})({})({}[)]}]"));
+    }
 
 }
